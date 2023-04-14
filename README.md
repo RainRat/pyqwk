8241337a3039951f03eeea33559c15ab4fcec6ce
RainRat <rainrat78@yahoo.ca>
2023-04-14 14:02:37 -0700

    add noheader, truncatesignatures and cutquoting options

--- README.md
+++ README.md
@@ -1,29 +1,66 @@
 # pyqwk
- .qwk reader in python
 
+pyqwk is a .qwk reader in Python that exports .QWK mail archives to a more readable format, without requiring the use of a .QWK reader. This is useful for data archivists who want to archive .QWK mail archives in a more readable format. .QWK archives were popular back in the Fidonet and BBS days and people may want to archive them now.
 
-For data archivists that want to archive .QWK mail archives in a more readable format. It was popular back in the Fidonet and BBS days, and people may want to archive them now.
+## Usage
 
-Exports the entire messages.dat to a more readable format without setting up a .QWK reader, which might not even have a function to export the entire file.
+pyqwk exports the entire `messages.dat` file to a more readable format. It can take either the `messages.dat` file or the `.qwk` file:
 
-.QWK archives are a ZIP file, which contains the messages in messages.dat, plus some index files. This program can take either the messages.dat or the .qwk file:
+- `messages.dat`: The indexes aren't needed, but the only downside is that it doesn't have the names of the subboards the messages are from.
+- `.qwk` file: It will use the `CONTROL.DAT` file to retrieve the names of the conferences.
 
-messages.dat: The indexes aren't needed, the only downside is that it doesn't have the names of the subboards the messages are from.
+To use pyqwk, follow these steps:
 
-.qwk file: It will use the CONTROL.DAT to retrieve the names of the conferences.
+1. Just put qwk.py where you want to run it from. The struct, zipfile, and argparse libraries that it uses are all included in standard python.
+2. Run pyqwk with the path to either the `messages.dat` file or the `.qwk` file. For example:
 
-Known issue: Some .qwk packets from this era use a ZIP compression method that modern Python doesn't know.
-  
-Workaround 1: Some archive utilities have an tool to repack archives into modern formats. Some even have a method to do it in bulk.
+```
+python qwk.py messages.dat
+```
 
-Workaround 2: Unpack the archive and act on messages.dat
+or
 
-For each message, the headers aren't exported in the same order they appear in messages.dat; I rearrange them to an order that might make more sense to a modern reader.
+```
+python qwk.py my_archive.qwk
+```
 
-It'll leave out message ids, which probably aren't useful to a modern reader, but use --verbose if you want them.
+For each message, the headers aren't exported in the same order they appear in `messages.dat`; they are rearranged to an order that might make more sense to a modern reader. 
 
-It'll leave out messages marked as private. If you are an archivist, you could leave that as is, to not include personal messages from people kind enough to donate their QWK packets. But if you're archiving your own, use --private to include them.
+## Options
 
-Apparently there's a password protection option for messages. I've never heard of that until now. They'll be skipped.
+- `--verbose` or `-v`
+pyqwk will leave out message IDs, which probably aren't useful to a modern reader, but use `--private` if you want them.
 
-This was late night coding binge. probably has bugs. will accept pull requests.
+- `--private` or `-p`
+pyqwk will also leave out messages marked as private. If you are an archivist, you could leave that as is to not include personal messages from people who kindly donated their `.qwk` packets. But if you're archiving your own, use `--private` to include them.
+
+- `--noheader` or `-n`
+Leave out the message header.
+
+- `--truncatesignatures` or `-t`
+Truncate each message at the signature (everything after a line that consists only of "---" or starts with " * ")
+
+- `--cutquoting` or `-c`
+Delete quoted text (that uses ">" as quoting character).
+
+## Known Issues
+
+- Some `.qwk` packets from this era use a ZIP compression method that modern Python doesn't know. To work around this issue:
+
+  - Some archive utilities have a tool to repack archives into modern formats. Some even have a method to do it in bulk.
+  - Unpack the archive and act on `messages.dat`.
+
+- Apparently, there's a password protection option for messages, but pyqwk skips those messages. 
+
+- `cutquoting` is simplistic
+  - Only knows quoting character `>`
+  - Can miss quoting that has been word wrapped, ie.
+```
+XX> This is actually a pretty long line that has been quoted and then word
+wrap
+XX> has made "wrap" not recognized as part of a quote.
+```
+
+## Contributing
+
+Pull requests are accepted.
--- qwk.py
+++ qwk.py
@@ -2,15 +2,22 @@ import sys
 import argparse
 import zipfile
 import struct
+import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file1', help='the messages.dat filename, or the QWK packet', nargs='?', default='messages.dat')
 parser.add_argument('file2', help='the output filename. default is console', nargs='?')
 parser.add_argument('-v', '--verbose', help='verbose output. export message id fields that may not be relevant', action='store_true')
 parser.add_argument('-p', '--private', help='export messages marked private', action='store_true')
+parser.add_argument('-n', '--noheader', help='leave out message header', action='store_true')
+parser.add_argument('-t', '--truncatesignatures', help='truncate at signatures (everything after a line that consists only of "---" or starts with " * ")', action='store_true')
+parser.add_argument('-c', '--cutquoting', help='delete quoted text (that uses ">" as quoting character)', action='store_true')
 args = parser.parse_args()
 verbose=args.verbose
 exportPrivate=args.private
+noHeader=args.noheader
+truncateSignatures=args.truncatesignatures
+cutQuoting=args.cutquoting
 
 boarddict={}
 if zipfile.is_zipfile(args.file1):
@@ -19,7 +26,7 @@ if zipfile.is_zipfile(args.file1):
     controlname=''
     with zipfile.ZipFile(args.file1) as myzip:
         file_list = myzip.namelist()
-        for file_name in file_list: #workaround: zipfile library is case sensitive. loop through case-insensitive and fine how they are capitalized 
+        for file_name in file_list: #workaround: zipfile library is case sensitive. loop through case-insensitive and find how they are capitalized 
             if file_name.lower()=='messages.dat':
                 messagesname=file_name
             if file_name.lower()=='control.dat':
@@ -45,7 +52,6 @@ for i in range(0, len(data), 128):
             sys.exit('not a messages.dat file')
         continue
     if intBlocks==0:
-        messagebuffer='--------------------------------------------------------------------------------\r\n'
         status, msgnum, msgdate, msgtime, msgto, msgfrom, msgsubject, msgpassword, refnum, numblocks, msgflag, confnum, lognum, nettag = struct.unpack('<c7s8s5s25s25s25s12s8s6scHHc', record)
         messageType=status.decode('latin1')
         isPassword=False
@@ -66,17 +72,20 @@ for i in range(0, len(data), 128):
             conf_name = str(confnum)
             not_found_flag=True
 
-        if verbose==True or not_found_flag==False:
-            messagebuffer+=('Conference: '+str(conf_name)+'\r\n')
-        if verbose==True:
-            messagebuffer+=('Message number: '+msgnum.decode('latin1')+'                    ')
-        messagebuffer+=('Date: '+msgdate.decode('latin1')+' '+msgtime.decode('latin1')+'\r\n')
-        messagebuffer+=('From: '+msgfrom.decode('latin1')+'\r\n')
-        messagebuffer+=('To: '+msgto.decode('latin1')+'\r\n')
-        messagebuffer+=('Subject: '+msgsubject.decode('latin1')+'\r\n')
-        if verbose==True:
-            messagebuffer+=('Reference number: '+refnum.decode('latin1')+'\r\n')
-        messagebuffer+='\r\n'
+        messagebuffer=''
+        if not noHeader:
+            messagebuffer+='--------------------------------------------------------------------------------\r\n'
+            if verbose==True or not_found_flag==False:
+                messagebuffer+=('Conference: '+str(conf_name)+'\r\n')
+            if verbose==True:
+                messagebuffer+=('Message number: '+msgnum.decode('latin1')+'                    ')
+            messagebuffer+=('Date: '+msgdate.decode('latin1')+' '+msgtime.decode('latin1')+'\r\n')
+            messagebuffer+=('From: '+msgfrom.decode('latin1')+'\r\n')
+            messagebuffer+=('To: '+msgto.decode('latin1')+'\r\n')
+            messagebuffer+=('Subject: '+msgsubject.decode('latin1')+'\r\n')
+            if verbose==True:
+                messagebuffer+=('Reference number: '+refnum.decode('latin1')+'\r\n')
+            messagebuffer+='\r\n'
         tempblocks=numblocks.decode('latin1').strip()
         intBlocks=int(tempblocks)-1
     else:
@@ -87,7 +96,17 @@ for i in range(0, len(data), 128):
         intBlocks=intBlocks-1
         if intBlocks==0:
             if (exportPrivate==True or isPrivate==False) and isPassword==False:
-                 fullmessagebuffer+=messagebuffer
+                lines = messagebuffer.splitlines()
+                quotePattern = r'^\s*[A-Z]{0,2}>'
+                new_lines = []
+                for line in lines:
+                    if (line == "---" or line.startswith(" * ")) and truncateSignatures:
+                        break
+                    if cutQuoting and re.match(quotePattern, line):
+                        continue
+                    new_lines.append(line)
+                fullmessagebuffer+='\r\n'.join(new_lines)+'\r\n'
+
 
 if args.file2 ==None:
     print (fullmessagebuffer)
