9f3294fd99f78ded4d840fb2443d2772369e2b93
Chris <chris@example.com>
2026-06-15 00:28:32 -0700

    Clean up unused tkinter.font import in GUI

--- pyqwk/gui.py
+++ pyqwk/gui.py
@@ -6,7 +6,6 @@ import logging
 import os
 import webbrowser
 import tkinter as tk
-from tkinter import font
 from collections import Counter
 from dataclasses import replace
 from tkinter import filedialog, messagebox, ttk, simpledialog
